186656b18f65ddadba967142a4e675a2a625ff57
Saul Moreno Abril <93sauu@gmail.com>
2019-06-10 20:42:39 +0200

    Track the Page in a SwiftUI View’s State

--- Landmarks/Views/PageView.swift
+++ Landmarks/Views/PageView.swift
@@ -10,19 +10,25 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var viewControllers: [UIHostingController<Page>]
+    @State var currentPage = 0
     
     init(_ views: [Page]) {
         self.viewControllers = views.map { UIHostingController(rootView: $0) }
     }
     
     var body: some View {
-        PageViewController(controllers: viewControllers)
+        VStack {
+            PageViewController(controllers: viewControllers, currentPage: $currentPage)
+            Text("Current Page: \(currentPage)")
+        }
     }
 }
 
+#if DEBUG
 struct PageView_Preview: PreviewProvider {
     static var previews: some View {
         PageView(features.map { FeatureCard(landmark: $0) })
             .aspectRatio(3/2, contentMode: .fit)
     }
 }
+#endif
