d38b44582fe46dce3b17d8201c325ffc3d624a54
Saul Moreno Abril <93sauu@gmail.com>
2019-06-10 20:18:46 +0200

    Delay Edit Propagation

--- Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Profile/ProfileHost.swift
@@ -16,16 +16,29 @@ struct ProfileHost : View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if self.mode?.value == .active {
+                    Button(action: {
+                        self.profile = self.draftProfile
+                        self.mode?.animation().value = .inactive
+                    }) {
+                        Text("Done")
+                    }
+                }
+                
                 Spacer()
                 
                 EditButton()
             }
+            
             if self.mode?.value == .inactive {
                 ProfileSummary(profile: profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onDisappear {
+                        self.draftProfile = self.profile
+                    }
             }
-            ProfileSummary(profile: profile)
+            
         }
         .padding()
     }
