033f2bf05da2a2ab766de46c4dd678794e54b963
Saul Moreno Abril <93sauu@gmail.com>
2019-06-10 19:29:38 +0200

    Compose HomeView

---
+++ Landmarks/Supporting Views/CategoryRow.swift
@@ -0,0 +1,57 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Saul Moreno Abril on 10/06/2019.
+//  Copyright © 2019 Saul Moreno Abril. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow : View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(showsHorizontalIndicator: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+                }
+                .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark
+                .image(forSize: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+            }
+            .padding(.leading, 15)
+    }
+}
+
+#if DEBUG
+struct CategoryRow_Previews : PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
+#endif
---
+++ Landmarks/Views/Home.swift
@@ -0,0 +1,57 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Saul Moreno Abril on 10/06/2019.
+//  Copyright © 2019 Saul Moreno Abril. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        .init(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                ForEach(categories.keys.sorted().identified(by: \.self)) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationBarTitle(Text("Featured"))
+        }
+        
+        
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image(forSize: 250).resizable()
+    }
+}
+
+#if DEBUG
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
+#endif
