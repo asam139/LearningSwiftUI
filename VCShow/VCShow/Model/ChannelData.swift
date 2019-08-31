e613862932b6c309734ec30148afe1216d1469b8
Saul Moreno Abril <93sauu@gmail.com>
2019-08-31 11:45:56 +0200

    VCShow: initial commit

---
+++ VCShow/VCShow/Model/ChannelData.swift
@@ -0,0 +1,9 @@
+//
+//  ChannelData.swift
+//  VCShow
+//
+//  Created by Saul Moreno Abril on 31/08/2019.
+//  Copyright © 2019 Saul Moreno Abril. All rights reserved.
+//
+
+import Foundation
---
+++ VCShow/VCShow/Views/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  VCShow
+//
+//  Created by Saul Moreno Abril on 31/08/2019.
+//  Copyright © 2019 Saul Moreno Abril. All rights reserved.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
